d7ac6053e75e00a8afa4847b687ce2ff2be4d9f8
HÀO HOÀNG <haohoangofficial@gmail.com>
2023-03-19 23:29:35 +0700

    new update

--- index.js
+++ index.js
@@ -34,11 +34,12 @@ const fetch_url = async ({ url, method, headers, body }) => {
           headers: headers,
           data: body,
       };
-      const source = axios.request(url, requestOptions)
+      const source = await axios.request(url, requestOptions)
+      const response = await source.data
       console.log(response);
       return {
           status: true,
-          message: source,
+          message: response,
           error: null
       }
 
